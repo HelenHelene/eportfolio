00dcd39f1149a793cfae6cc098d287dab55f15e2
Helen <110706633+HelenHelene@users.noreply.github.com>
2024-05-20 16:27:14 +0800

    Add files via upload

---
+++ code/OOP_DriverlessCar/emergency_brake.py
@@ -0,0 +1,11 @@
+class EmergencyBrake:
+    """Class for handling emergency brake."""
+
+    def __init__(self):
+        """Initial emergency brake system"""
+        self.emergency_brake = False
+
+    def activate_emergency_brake(self):
+        """Activate emergency brake."""
+        self.emergency_brake = True
+        print("Emergency brake activated")
---
+++ code/OOP_DriverlessCar/vehicle.py
@@ -0,0 +1,20 @@
+class Vehicle:
+    """Base class for all vehicles."""
+
+    def __init__(self, speed=0):
+        """Initial speed of vehicles."""
+        self.speed = speed
+
+    def start(self):
+        """Start vehicle engine."""
+        print("Engine started")
+        self.speed = 0
+
+    def stop(self):
+        """Stop vehicle engine."""
+        print("Engine stopped")
+        self.speed = 0
+
+    def __str__(self):
+        """Return vehicle current speed as string"""
+        return f"Vehicle speed: {self.speed} km/h"
